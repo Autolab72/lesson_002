c0289e224c1ae19f73da694a967e91980bf33dc2
Autolab72 <Autolab72@yandex.ru>
2021-09-13 23:25:19 +0500

    my_family OK

--- 04_my_family.py
+++ 04_my_family.py
@@ -4,17 +4,25 @@
 # Создайте списки:
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
-my_family = []
+
+my_family = ['Mother', 'Father', 'Brother', 'I\'m']
+Mother = my_family[0]
+Father = my_family[1]
+Brother = my_family[2]
+im = my_family[3]
 
 
 # список списков приблизителного роста членов вашей семьи
-my_family_height = [
-    # ['имя', рост],
-    [],
-]
+
+my_family_height = [[Mother, 165], [my_family[0], 165], [Father, 180], [Brother, 175]]
 
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
 
+print(my_family_height[2])
+print(my_family_height[1])
+print(my_family_height[0])
+
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+print(my_family_height[0][1] + my_family_height[1][1] +my_family_height[2][1] +my_family_height[3][1])
